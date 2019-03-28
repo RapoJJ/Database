6555414742b6499ef150122cb2d1e6e70b836fa1
udwee <jonttu.92@hotmail.com>
2019-03-28 14:30:47 +0200

    BankApp initialized

---
+++ BankAppDB/BankAppDB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankAppDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
