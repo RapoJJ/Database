54570271141b8be0079caa0ae7f098856ff18f4c
udwee <jonttu.92@hotmail.com>
2019-03-28 15:28:13 +0200

    IBankRepository built and BankRepository Create method done

---
+++ BankAppDB/BankAppDB/Repositories/BankRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BankAppDB.Models;
+
+namespace BankAppDB.Repositories
+{
+    class BankRepository : IBankRepository
+    {
+        private readonly BankdbContext _bankdbContext = new BankdbContext();
+
+        public void Create(Bank bank)
+        {
+            _bankdbContext.Add(bank);
+            _bankdbContext.SaveChanges();
+        }
+
+        public List<Bank> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bank ReadById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(long id, Bank bank)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
