10cbc5a7e6aa78a13704b3afdf7913523af67edb
udwee <jonttu.92@hotmail.com>
2019-04-09 10:05:18 +0300

    CustomerRepository CRUD done

--- BankAppDB/BankAppDB/Repositories/CustomerRepository.cs
+++ BankAppDB/BankAppDB/Repositories/CustomerRepository.cs
@@ -1,35 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using BankAppDB.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BankAppDB.Repositories
 {
     class CustomerRepository : ICustomerRepository
     {
+        private readonly BankdbContext _bankdbContext = new BankdbContext();
+
         public void Create(Customer customer)
         {
-            throw new NotImplementedException();
+            _bankdbContext.Add(customer);
+            _bankdbContext.SaveChanges();
         }
 
         public List<Customer> ReadAll()
         {
-            throw new NotImplementedException();
+            var customers = _bankdbContext.Customer.ToListAsync().Result;
+
+            return customers;
         }
 
         public Customer ReadById(long id)
         {
-            throw new NotImplementedException();
+            var customer = _bankdbContext.Customer
+                .Include(c => c.Account)
+                .ThenInclude(c => c.Transaction)
+                .FirstOrDefault(c => c.ID == id);
+            return customer;
         }
 
         public void Update(long id, Customer customer)
         {
-            throw new NotImplementedException();
+            var isAlive = ReadById(id);
+
+            if (isAlive != null)
+            {
+                _bankdbContext.Update(customer);
+                _bankdbContext.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine($"Didn't find customer with id: {id}.");
+            }
         }
 
         public void Delete(long id)
         {
-            throw new NotImplementedException();
+            var deletedCustomer = ReadById(id);
+
+            if (deletedCustomer != null)
+            {
+                _bankdbContext.Customer.Remove(deletedCustomer);
+                _bankdbContext.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine($"Didn't find customer with id: {id}.");
+            }
         }
     }
 }
