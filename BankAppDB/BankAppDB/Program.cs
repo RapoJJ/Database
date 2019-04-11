32ae103d2e3bcfc23967d41ad0f39400b707f667
udwee <jonttu.92@hotmail.com>
2019-04-11 11:19:37 +0300

    test doesnt work

--- BankAppDB/BankAppDB/Program.cs
+++ BankAppDB/BankAppDB/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using BankAppDB.Models;
 using BankAppDB.Repositories;
 
 namespace BankAppDB
@@ -15,44 +14,13 @@ namespace BankAppDB
 
             foreach (var b in bankList)
             {
-                Console.WriteLine($"{b.Name} {b.BIC} {b.Id}");
-
-                foreach (var c in b.Account)
-                {
-                    Console.WriteLine($"Account name: {c.Name} Balance: {c.Balance}");
-                }
+                Console.WriteLine($"{b.Name} {b.Bic} {b.ID}");
             }
 
 
             var bank = bankRepository.ReadById(1);
 
             Console.WriteLine(bank.Name);
-
-
-            /*Bank newBank = new Bank
-            {
-                Name = "Nordea",
-                BIC = "NDEAFIHH"
-            };*/
-            //bankRepository.Create(newBank);
-
-            //Bank updateBank = bankRepository.ReadById(3);
-
-            //updateBank.BIC = "NDEAFIHH";
-
-            //bankRepository.Update(3,updateBank);
-
-            //bankRepository.Delete(5);
-        }
-
-        private static ConsoleKeyInfo UserInterface()
-        {
-            Console.WriteLine("Choose which data you want to view/edit:");
-            Console.WriteLine("[1] Banks");
-            Console.WriteLine("[2] Customers");
-            Console.WriteLine("[Esc] Close the program.");
-            Console.Write("Press key of your choice: ");
-            return Console.ReadKey();
         }
     }
 }
